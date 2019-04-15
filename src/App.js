328da577a20d2166f627bf12c7efb28200b6cd72
Rayhan96Shuvo <rayhan96shuvo@gmail.com>
2019-04-15 16:52:23 -0500

    Router and structure set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Components/Dashboard/dashboard';
+import Fanexperience from './Components/FanExperience/fanexperience';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      //Fan experience page. This is the first page that will load. For Dashboard access for now 
+      //we will have to add /dashboardRAAMforVIPaccess to out http
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Fanexperience} />
+          <Route path="/dashboardRAAMforVIPaccess" component={Dashboard} />
+        </div>
+      </BrowserRouter>
+
+
     );
   }
 }

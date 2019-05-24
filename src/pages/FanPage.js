20ce4f062a94bd35add93a25867713f1e9e79f31
Andrew Geng <andrew.geng@Andrews-MacBook-Pro.local>
2019-05-23 19:11:08 -0700

    Refractor and Added Footers

--- src/pages/FanPage.js
+++ src/pages/FanPage.js
@@ -1,42 +1,30 @@
 import React, { Component } from 'react';
 import {Container, Row, Col} from "reactstrap";
 import MapBoxComp from "../Components/MapBox Components/MapBox";
+import MapBoxmain from "../Components/MapBox Components/Mapboxmain";
 import Navagation from "../Components/Navagation";
+import Widget from "../Components/Widgets/Widget";
+import Footer from "../Components/Footer";
+
+import "../css/fanexperience.css";
+import "../css/stylesheet.css";
 
 class FanPage extends Component {
     render() { 
         return (
-            <div id="FanPageWrapper">
+            <div id="FanPageWrapper" style={{background: "#2c2954", paddingBottom: "30px" }}>
                 <Navagation />
-                <Container fluid="true">
-                    <Row>
-                    <Col xs="1"></Col>
-                    <Col xs="10">
-                        <Row>
-                            <Col style={{maxWidth: "70%"}}>
-                                <MapBoxComp />
-                            </Col>
-                            <Col style={{maxWidth: "20%", background: "red"}}>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col style={{maxWidth: "50%", background: "purple"}}>
-                                <MapBoxComp />
-                            </Col>
-                            <Col style={{maxWidth: "50%", background: "green"}}>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col style={{maxWidth: "50%", background: "blue"}}>
-                                <MapBoxComp />
-                            </Col>
-                            <Col style={{maxWidth: "50%", background: "orange"}}>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col xs="1"></Col>
-                    </Row>
+                <MapBoxmain />
+                <br />
+                <Container className="App" fluid="true">
+                    <Widget heading="Stat 1" rowspan={3} />
+                    <Widget heading="Stat 2"/>
+                    <Widget heading="Some graph shit" colspan={2} rowspan={2}/>
+                    <Widget heading="Some more stat"/>
+                    <Widget heading="ThisAndThat"/>
+                    <Widget heading="RAAM"/>
                 </Container>
+                <Footer />
             </div> 
          );
     }

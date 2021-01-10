4dc475e4200fca715a96311615ffe6701565f213
Jessica Miyake <jmiyake@wisc.edu>
2021-01-10 17:36:23 -0600

    fixed event page compliation errors

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import {Container, Row, Col, Button} from "reactstrap";
-import Navagation from "../components/Navagation";
-import MapBoxmain from "../components/MapBox Components/Mapboxmain";
+import Navagation from "../Components/Navagation";
+import MapBox from "../Components/MapBox Components/MapBox";
 import "../css/stylesheet.css";
 
 class EventPage extends Component {
@@ -9,33 +8,11 @@ class EventPage extends Component {
         return (
             <div>
             <Navagation />
-            <Container style={{paddingTop: "2rem"}}>
-                <Row>
-                    <Col xs="1"></Col>
-                    <Col><h1 style = {{textAlign: "center"}}>Events</h1></Col>
-                    <Col xs="1"></Col>
-                </Row>
-                <Row style={{paddingTop: "2rem"}}>
-                    <Col xs="1"></Col>
-                    <Col><MapBoxmain /></Col>
-                    <Col xs="1"></Col>
-                </Row>
-                <Row style={{paddingTop: "2rem"}}>
-                    <Col xs="1"></Col>
-                    <Col><div style = {{textAlign: "center"}}>User participation meter</div></Col>
-                    <Col xs="1"></Col>
-                </Row>
-                <Row>
-                    <Col xs="1"></Col>
-                    <Col><div style = {{textAlign: "left"}}>Why pledge this athlete?</div></Col>
-                    <Col xs="1"></Col>
-                </Row>
-                <Row>
-                    <Col xs="1"></Col>
-                    <Col><div style = {{textAlign: "left"}}>User interaction option</div></Col>
-                    <Col xs="1"></Col>
-                </Row>
-            </Container>
+                    <h1 style = {{textAlign: "center"}}>Events</h1>
+                    <MapBox />
+                    <div style = {{textAlign: "center"}}>User participation meter</div>
+                    <div style = {{textAlign: "center"}}>Why pledge this athlete?</div>
+                    <div style = {{textAlign: "center"}}>User interaction option</div>
             </div>
         )
     }

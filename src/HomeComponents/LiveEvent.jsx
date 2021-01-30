41f12a8f3d59ee4b4bdbdfd2e8638cd0ab00b83a
Reed Nelson <reed.nelson.d@gmail.com>
2021-01-30 16:17:18 -0600

    Swap homepage mapbox for garmin map

--- src/HomeComponents/LiveEvent.jsx
+++ src/HomeComponents/LiveEvent.jsx
@@ -9,7 +9,21 @@ class LiveEvent extends Component {
     return (
       <div className="Live-Event row">
         <div className="col Live-Event-Map-Box">
-          <MapBox />
+          {/* USE MAPBOX FOR ALL EVENTS AFTER RWANDA */}
+          {/* <MapBox /> */}
+
+          {/* V REMOVE GARMIN MAP WHEN RWANDA IS FINISHED V */}
+          <div className="Garmin-Map">
+            <iframe
+              src="https://share.garmin.com/share/lespoulettesfrites"
+              frameborder="0"
+              marginwidth="0"
+              marginheight="0"
+              width="100%"
+              height="760"
+            ></iframe>
+          </div>
+          {/* ^ REMOVE GARMIN MAP WHEN RWANDA IS FINISHED ^ */}
         </div>
         <div className="col Live-Event-Feed-Box">Live Feed</div>
       </div>

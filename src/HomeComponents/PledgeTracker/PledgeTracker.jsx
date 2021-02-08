afc89f107583c776758eacdb37843181c5400fd2
Reed Nelson <reed.nelson.d@gmail.com>
2021-02-08 16:40:47 -0600

    tweak pledge bar

--- src/HomeComponents/PledgeTracker/PledgeTracker.jsx
+++ src/HomeComponents/PledgeTracker/PledgeTracker.jsx
@@ -3,17 +3,17 @@ import PledgeTrackerBar from "./PledgeTrackerComponent";
 
 import "../../css/Styles.css";
 
-const pledges = 0;
+const pledges = 20000;
 
 class PledgeTracker extends Component {
   render() {
     return (
       <div className="Pledge-Tracker">
-        <h2>Pledges</h2>
+        <h2>Progress</h2>
         <PledgeTrackerBar pledges={pledges / 500} />
         <span className="Pledge-Tracker-Description">
-          <strong>{pledges}</strong> people have made the{" "}
-          <strong>PLEDGE</strong>. Now it's your turn!
+          <strong>{"20,000"}</strong> people have made the PLEDGE. Now it's{" "}
+          <em>your</em> move!
         </span>
       </div>
     );

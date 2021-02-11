ec25be1c8854f21a96aae600cb73cfc3a78a0efb
Reed Nelson <reed.nelson.d@gmail.com>
2021-02-10 19:57:55 -0600

    allow click anywhere on athlete card

--- src/HomeComponents/Gallery/GalleryCard.jsx
+++ src/HomeComponents/Gallery/GalleryCard.jsx
@@ -6,19 +6,21 @@ class GalleryCard extends Component {
   render() {
     return (
       <div className="Event-Gallery-Card">
-        <img
-          className="Event-Gallery-Card-Image"
-          src={this.props.img}
-          alt={this.props.name}
-        ></img>
-        <div className="Event-Gallery-Card-Info">
-          <span className="Event-Gallery-Card-Name">
-            <a className="Event-Link" href={this.props.link}>
-              {this.props.name}
-            </a>
-          </span>
-          <span className="Event-Gallery-Card-Bio">{this.props.event}</span>
-        </div>
+        <a href={this.props.link}>
+          <img
+            className="Event-Gallery-Card-Image"
+            src={this.props.img}
+            alt={this.props.name}
+          ></img>
+          <div className="Event-Gallery-Card-Info">
+            <span className="Event-Gallery-Card-Name">
+              <a className="Event-Link" href={this.props.link}>
+                {this.props.name}
+              </a>
+            </span>
+            <span className="Event-Gallery-Card-Bio">{this.props.event}</span>
+          </div>
+        </a>
       </div>
     );
   }

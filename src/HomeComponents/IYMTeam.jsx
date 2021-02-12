6b55a9197704db46c879abc748aae3bb3e176092
Matt Wildman <mjwildman15@gmail.com>
2021-02-11 20:09:36 -0600

    Title and photo sorting

--- src/HomeComponents/IYMTeam.jsx
+++ src/HomeComponents/IYMTeam.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../css/Styles.css";
 import WHO from "../images/Logos/who.png";
-import DB from "../images/Team Pics/DAB.jpg";
+import DB from "../images/Team Pics/DougBarton.jpg";
 import BL from "../images/Team Pics/Benjamin L.jpg";
 import DR from "../images/Team Pics/Douglas R.jpg";
 import JM from "../images/Team Pics/Jessica Miyake.png";
@@ -10,7 +10,15 @@ import RN from "../images/Team Pics/Reed Nelson.jpg";
 import SW from "../images/Team Pics/Steve Wangen.jpg";
 import TM from "../images/Team Pics/Ted M.jpg";
 import TH from "../images/Team Pics/Trisha Hernandez.jpg";
+import RW from "../images/Team Pics/RyanW.jpg";
 import PL from "../images/Team Pics/placeholder.png";
+import CL from "../images/Team Pics/ChrisLewis.jpg";
+import AC from "../images/Team Pics/AdamC.jpg";
+import CD from "../images/Team Pics/CarsonD.jpg";
+
+
+
+
 class IYMTeam extends Component {
   state = {};
   render() {
@@ -25,7 +33,7 @@ class IYMTeam extends Component {
               <img className="Team-Picture-Doug" src={DB} alt="DB" />
               <p className="Team-Content">
                 <p className="Team-Name">Doug Barton</p>
-                <p>Director of E-Buisness Consortium at UW Madison</p>
+                <p>Director of E-Buisness Consortium at UW-Madison</p>
                 <a href="https://www.linkedin.com/in/bartondoug/">Linkedin</a>
                 <br></br>
               </p>
@@ -44,8 +52,7 @@ class IYMTeam extends Component {
               <p className="Team-Content">
                 <p className="Team-Name">Matt Wildman</p>
                 <p>
-                  Incoming SWE Intern @ Facebook | Project Coordinator at
-                  UW-Madison Undergraduate Project Lab
+                  Computer Science at UW-Madison | Undergraduate Project Lab Coordinator
                 </p>
                 <a href="https://www.linkedin.com/in/matt-wildman/">Linkedin</a>
                 <br></br>
@@ -61,7 +68,7 @@ class IYMTeam extends Component {
               </p>
             </div>
             <div className="col-sm-4">
-              <img className="Team-Picture" src={PL} alt="PL" />
+              <img className="Team-Picture" src={RW} alt="RW" />
               <p className="Team-Content">
                 <p className="Team-Name">Ryan West</p>
                 <p>Co-Founder, Head of Applied Science at Idio Analytics</p>
@@ -72,13 +79,11 @@ class IYMTeam extends Component {
               </p>
             </div>
             <div className="col-sm-4">
-              <img className="Team-Picture" src={PL} alt="PL" />
+              <img className="Team-Picture" src={CL} alt="CL" />
               <p className="Team-Content">
                 <p className="Team-Name">Chris Lewis</p>
                 <p>
-                  Senior Product Manager | Market Analysis Consultant | Agile
-                  Product Owner | Solutions Architect Lead | Product Management
-                  Partner | Technical Leader
+                  Senior Product Manager at Dell
                 </p>
                 <a href="https://www.linkedin.com/in/chris-lewis-1007248/">
                   Linkedin
@@ -87,7 +92,7 @@ class IYMTeam extends Component {
               </p>
             </div>
             <div className="col-sm-4">
-              <img className="Team-Picture" src={PL} alt="PL" />
+              <img className="Team-Picture" src={AC} alt="AC" />
               <p className="Team-Content">
                 <p className="Team-Name">Adam Converse</p>
                 <p>Senior Software Engineer at Zendesk</p>
@@ -101,7 +106,7 @@ class IYMTeam extends Component {
               <img className="Team-Picture" src={RN} alt="RN" />
               <p className="Team-Content">
                 <p className="Team-Name">Reed Nelson</p>
-                <p>CS Student at UW-Madison</p>
+                <p>Computer Science at UW-Madison | Undergraduate Project Lab Coordinator</p>
                 <a href="https://www.linkedin.com/in/reed-nelson-a24ba2196?trk=public_profile_browsemap_profile-result-card_result-card_full-click">
                   Linkedin
                 </a>
@@ -112,22 +117,17 @@ class IYMTeam extends Component {
               <img className="Team-Picture" src={JM} alt="JM" />
               <p className="Team-Content">
                 <p className="Team-Name">Jessica Miyake</p>
-                <p>CS Student at UW-Madison</p>
-                <a
-                  href="
-https://www.linkedin.com/in/jessicamiyake/"
-                >
-                  Linkedin
-                </a>
+                <p>UW-Madison Computer Science and Digital Studies Alumni</p>
+                <a href="https://www.linkedin.com/in/jessicamiyake/">  Linkedin </a>
                 <br></br>
               </p>
             </div>
             <div className="col-sm-4">
-              <img className="Team-Picture" src={PL} alt="PL" />
+              <img className="Team-Picture" src={CD} alt="CD" />
               <p className="Team-Content">
                 <p className="Team-Name">Carson Drury</p>
-                <p>CS Student at UW-Madison</p>
-                <a href="insert Linkedin">Linkedin</a>
+                <p>Computer Science at UW-Madison | Undergraduate Project Lab Coordinator</p>
+                <a href="https://www.linkedin.com/in/carson-drury/">Linkedin</a>
                 <br></br>
               </p>
             </div>
@@ -135,7 +135,7 @@ https://www.linkedin.com/in/jessicamiyake/"
               <img className="Team-Picture" src={PL} alt="PL" />
               <p className="Team-Content">
                 <p className="Team-Name">Ivan Hu</p>
-                <p>CS Student at UW-Madison</p>
+                <p>Computer Science at UW-Madison</p>
                 <a href="insert Linkedin">Linkedin</a>
                 <br></br>
               </p>
